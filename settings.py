db542a63bd2cac78c34845a46a1b97f2f104e8a3
Owen Cassidy <ravrahn@ravrahn.net>
2014-02-23 13:59:03 +1100

    Added admin settings

--- settings.py
+++ settings.py
@@ -91,7 +91,7 @@ USE_SOUTH = True
 # In the format (('Full Name', 'email@example.com'),
 #                ('Full Name', 'anotheremail@example.com'))
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    ('Owen Cassidy', 'ravrahn@gmail.com'),
 )
 MANAGERS = ADMINS
 
