c6df17049a66772e8b7858ce89b9d02dbb3ec70d
Owen Cassidy <ravrahn@ravrahn.net>
2014-07-03 18:06:00 +1000

    Added allowed hosts

--- settings.py
+++ settings.py
@@ -99,7 +99,7 @@ MANAGERS = ADMINS
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "ravrahn.com", "127.0.0.1", "107.170.103.214"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
